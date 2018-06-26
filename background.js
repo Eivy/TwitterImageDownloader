2446cccac595b49d08b8decc10652e02db743b22
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-06-26 23:28:08 +0900

    Enable to listup images

--- background.js
+++ background.js
@@ -4,14 +4,23 @@ if (d) {
 }
 let container = document.createElement('div')
 container.id = 'imagedownloader'
+container.onclick = () => {
+	container.remove()
+}
 let images = document.createElement('div')
 images.className = 'downloader_images'
 document.querySelectorAll('.AdaptiveMedia-photoContainer').forEach(e => {
-	let box = document.createElement('div')
 	let copy = e.cloneNode(true)
-	box.appendChild(copy)
-	box.className = 'downloader_image_box'
-	images.appendChild(box)
+	copy.className = 'downloader_image_box'
+	copy.style.backgroundColor = ''
+	copy.onclick = (event) => {
+		event.stopPropagation()
+		console.log(copy.querySelector('img').src)
+		browser.downloads.download({
+			url: copy.querySelector('img').src + ':orig'
+		}).catch((reason) => { console.log(reason) })
+	}
+	images.appendChild(copy)
 })
 container.appendChild(images)
 document.body.appendChild(container)
