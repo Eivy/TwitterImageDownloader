eecc829d962a4c0ad03d3ce5b27f65e4d9f3ef23
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-06-27 13:01:50 +0900

    Add checkbox to each image box

--- inject.js
+++ inject.js
@@ -13,6 +13,10 @@ document.querySelectorAll('.stream img[src*="pbs.twimg.com/media"]').forEach(e =
 	let copy = e.cloneNode()
 	let box = document.createElement('div')
 	box.className = 'downloader_image_box'
+	let ch = document.createElement('input')
+	ch.type = 'checkbox'
+	ch.onclick = (event) => { event.stopPropagation() }
+	box.appendChild(ch)
 	box.appendChild(copy)
 	box.onclick = (event) => {
 		event.stopPropagation()
