9776f1a81ca35348b54ccf045ba87faeb6542ef6
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-07-30 22:01:07 +0900

    Enable to download videos

--- background.js
+++ background.js
@@ -3,13 +3,19 @@ chrome.browserAction.onClicked.addListener((tab) => {
 	chrome.tabs.insertCSS(tab.id, {file: 'base.css'})
 })
 
-async function download_item (request, sender, sendResponse) {
+var authorization = ''
+var csrf_token = ''
+
+function download_item (request) {
 	chrome.storage.local.get('path', function (result) {
 		for (let i of request) {
-			console.log(i)
 			let s = i.split('/')
+			let name = (result.path || '') + s[s.length - 1].replace(/:.*$/, '')
+			if (i.match('blob:')) {
+				name += '.ts'
+			}
 			try {
-				chrome.downloads.download({url: i, filename: (result.path || '') + s[s.length - 1]})
+				chrome.downloads.download({url: i, filename: name})
 			} catch (e) {
 				console.log(e)
 			}
@@ -17,4 +23,31 @@ async function download_item (request, sender, sendResponse) {
 	})
 }
 
-chrome.runtime.onMessage.addListener(download_item)
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.hasOwnProperty('id')) {
+		sendResponse({ authorization, csrf_token })
+		return false
+	}
+	console.log(request)
+	download_item(request)
+	return false
+})
+
+function getAuth (e) {
+	e.requestHeaders.forEach((h) => {
+		switch (h.name.toLowerCase()) {
+			case 'authorization':
+				authorization = h.value
+				break
+			case 'x-csrf-token':
+				csrf_token = h.value
+				break
+		}
+	})
+}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	getAuth,
+	{urls: ['https://*.twitter.com/*']},
+	['requestHeaders']
+)
--- inject.js
+++ inject.js
@@ -2,6 +2,10 @@ var d = document.querySelector('#imagedownloader')
 if (d) {
 	d.remove()
 }
+var auth = {}
+chrome.runtime.sendMessage({id: ''}, (value) => {
+	auth = value
+})
 let container = document.createElement('div')
 container.id = 'imagedownloader'
 container.onclick = () => {
@@ -17,12 +21,20 @@ container.onscroll = () => {
 let ok = document.createElement('button')
 ok.className = 'ok'
 ok.innerText = 'Download'
-ok.onclick = event => {
+ok.onclick = async event => {
 	event.stopPropagation()
 	let urls = []
-	document.querySelectorAll('#imagedownloader .downloader_image_box.selected img').forEach(e => {
+	document.querySelectorAll('#imagedownloader .downloader_image_box.selected.image img').forEach(e => {
 		urls.push(e.src + ':orig')
 	})
+	let videos = document.querySelectorAll('#imagedownloader .downloader_image_box.selected.video img')
+	for (let video of videos) {
+		if (video.hasOwnProperty('video_url')) {
+			urls.push(video.video_url)
+		} else {
+			urls.push(await getVideo(video.id))
+		}
+	}
 	chrome.runtime.sendMessage(urls)
 	close()
 }
@@ -44,6 +56,13 @@ container.appendChild(cancel)
 let images = document.createElement('div')
 images.className = 'downloader_images'
 container.appendChild(images)
+let videoView = document.createElement('div')
+videoView.className = 'video_viewer'
+videoView.onclick = event => {
+	event.stopPropagation()
+	videoView.classList.remove('shown')
+	videoView.innerHTML = ''
+}
 let shown = document.createElement('div')
 shown.className = 'image_viewer'
 shown.onclick = event => {
@@ -53,6 +72,7 @@ shown.onclick = event => {
 let img = document.createElement('img')
 shown.appendChild(img)
 container.appendChild(shown)
+container.appendChild(videoView)
 let more = document.createElement('div')
 more.className = 'get_more'
 more.innerText = '⏬'
@@ -69,36 +89,183 @@ observer.observe(document.querySelector('.stream-container'), {attributes: true}
 document.body.appendChild(container)
 
 function appendItems (target) {
-	document.querySelectorAll('.js-navigable-stream img[src*="pbs.twimg.com/media"]').forEach(e => {
-		if (document.querySelector('.downloader_image_box img[src="' + e.src + '"]')) {
-			return
+	document.querySelectorAll('.js-navigable-stream img[src*="pbs.twimg.com/media"], .js-navigable-stream .AdaptiveMedia div.PlayableMedia-player').forEach(async e => {
+		var item
+		if (e.tagName.toLowerCase() === 'div') {
+			item = getVideoItem(e)
+		} else if (e.tagName.toLowerCase() === 'img') {
+			item = getImageItem(e)
 		}
-		let item = document.createElement('img')
-		item.src = e.src
-		let box = document.createElement('div')
-		box.className = 'downloader_image_box'
-		let v = document.createElement('span')
-		v.className = 'view'
-		v.innerText = '👁'
-		v.onclick = event => {
-			event.stopPropagation()
-			shown.classList.add('shown')
-			img.src = e.src
+		if (item) {
+			target.appendChild(item)
 		}
-		box.appendChild(v)
-		box.appendChild(item)
-		box.onclick = event => {
-			event.stopPropagation()
-			if (box.classList.contains('selected')) {
-				box.classList.remove('selected')
-			} else {
-				box.classList.add('selected')
-			}
-			badge.innerText = document.querySelectorAll('#imagedownloader .downloader_image_box.selected img').length
-			badge.style.visibility = badge.innerText !== '0' ? 'visible' : 'hidden'
-		}
-		target.appendChild(box)
 	})
+}
+
+function getImageItem (e) {
+	if (document.querySelector('.downloader_image_box img[src="' + e.src + '"]')) {
+		return null
+	}
+	let item = document.createElement('img')
+	item.src = e.src
+	let box = document.createElement('div')
+	box.className = 'downloader_image_box'
+	box.classList.add('image')
+	let v = document.createElement('span')
+	v.className = 'view'
+	v.innerText = '👁'
+	v.onclick = event => {
+		event.stopPropagation()
+		shown.classList.add('shown')
+		img.src = e.src
+	}
+	box.appendChild(v)
+	box.appendChild(item)
+	box.onclick = event => {
+		event.stopPropagation()
+		if (box.classList.contains('selected')) {
+			box.classList.remove('selected')
+		} else {
+			box.classList.add('selected')
+		}
+		badge.innerText = document.querySelectorAll('#imagedownloader .downloader_image_box.selected img').length
+		badge.style.visibility = badge.innerText !== '0' ? 'visible' : 'hidden'
+	}
+	return box
+}
+
+function getVideoItem (e) {
+	if (document.querySelector('.downloader_image_box img[src="' + e.style.backgroundImage.slice(5, -2) + '"]')) {
+		return null
+	}
+	let item = document.createElement('img')
+	console.log(e)
+	console.log(e.style.backgroundImage)
+	item.src = e.style.backgroundImage.slice(5, -2)
+	while (e.parentNode.tagName.toLowerCase() !== 'li') {
+		e = e.parentNode
+	}
+	let id = e.getAttribute('data-tweet-id')
+	item.setAttribute('id', id)
+	let v = document.createElement('span')
+	v.className = 'view'
+	v.innerText = '📹'
+	v.onclick = (event) => { showVideo(event, e, id, item) }
+	let box = document.createElement('div')
+	box.className = 'downloader_image_box'
+	box.classList.add('video')
+	box.appendChild(v)
+	box.appendChild(item)
+	box.onclick = event => {
+		event.stopPropagation()
+		if (box.classList.contains('selected')) {
+			box.classList.remove('selected')
+		} else {
+			box.classList.add('selected')
+		}
+		badge.innerText = document.querySelectorAll('#imagedownloader .downloader_image_box.selected img').length
+		badge.style.visibility = badge.innerText !== '0' ? 'visible' : 'hidden'
+	}
+	return box
+}
+
+async function showVideo (event, e, id, item) {
+	event.stopPropagation()
+	console.log(e)
+	let video = document.createElement('video')
+	let url = await getVideo(id)
+	if (url !== '') {
+		video.src = url
+		item.video_url = url
+		videoView.appendChild(video)
+	}
+	video.setAttribute('controls', '')
+	video.setAttribute('autoplay', '')
+	video.setAttribute('loop', '')
+	videoView.classList.add('shown')
+}
+
+async function getVideo (id) {
+	console.log(auth)
+	let url = ''
+	let xhr = new XMLHttpRequest()
+	xhr.open('GET', 'https://api.twitter.com/1.1/statuses/show/' + id + '.json', false)
+	xhr.withCredentials = true
+	xhr.crossDomain = true
+	xhr.setRequestHeader('authorization', auth.authorization)
+	xhr.setRequestHeader('x-csrf-token', auth.csrf_token)
+	xhr.send()
+	let data = JSON.parse(xhr.responseText)
+	if (data.hasOwnProperty('extended_entities')) {
+		for (let m of data.extended_entities.media) {
+			let bitrate = 0
+			for (let v of m.video_info.variants) {
+				if (v.content_type === 'video/mp4' && v.bitrate > bitrate) {
+					bitrate = v.bitrate
+					url = v.url.split('?')[0]
+				}
+			}
+		}
+	} else {
+		xhr.open('GET', 'https://api.twitter.com/1.1/videos/tweet/config/' + id + '.json', false)
+		xhr.withCredentials = true
+		xhr.crossDomain = true
+		xhr.setRequestHeader('authorization', auth.authorization)
+		xhr.setRequestHeader('x-csrf-token', auth.csrf_token)
+		xhr.send()
+		data = JSON.parse(xhr.responseText)
+		if (data.track.playbackType === 'video/mp4') {
+			url = data.track.playbackUrl
+		} else if (data.track.playbackType === 'application/x-mpegURL') {
+			let urls = parseM3U8(data.track.playbackUrl)
+			let array = []
+			for (let i in urls) {
+				let response = await fetch(urls[i])
+				array.push(await response.arrayBuffer())
+			}
+			let length = 0
+			array.forEach(a => {
+				length += a.byteLength
+			})
+			let buffer = new Uint8Array(length)
+			let pos = 0
+			array.forEach(a => {
+				buffer.set(new Uint8Array(a), pos)
+				pos += a.byteLength
+			})
+			let blob = new Blob([buffer], {type: 'video/mp2t'})
+			url = window.URL ? window.URL.createObjectURL(blob) : window.webkitURL.createObjectURL(blob)
+			console.log(url)
+		}
+	}
+	return url
+}
+
+function parseM3U8 (url) {
+	var urls = getUrlsFromM3U8(url)
+	return urls
+}
+
+function getUrlsFromM3U8 (url) {
+	let list = []
+	let xhr = new XMLHttpRequest()
+	if (!url.match(/^https?:\/\//)) {
+		url = 'https://video.twimg.com' + url
+	}
+	xhr.open('GET', url, false)
+	xhr.send()
+	var lines = xhr.responseText.split(/\r?\n/)
+	lines.forEach(l => {
+		if (l.match(/^[^#].+\.ts$/)) {
+			if (l.match(/^\//)) {
+				l = 'https://video.twimg.com' + l
+			}
+			list.push(l)
+		} else if (l.match(/\.m3u8$/)) {
+			list = list.concat(getUrlsFromM3U8(l))
+		}
+	})
+	return list
 }
 
 function close () {
